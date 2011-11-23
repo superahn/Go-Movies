5681196f2fa4758ddc3b3b48b7db385e60aefb75
Sung Ahn <superahn@gmail.com>
2011-11-23 22:21:53 +1100

    remove api key

--- application/config/movie_settings.php
+++ application/config/movie_settings.php
@@ -5,7 +5,7 @@
 | API Key for Rotten Tomatoes
 |--------------------------------------------------------------------------
 */
-$config['api_key']	= '6bwd3wxq83gxcg9ttd7mu4yr';
+$config['api_key']	= 'your api key';
 
 /* End of file movie_sessings.php */
 /* Location: ./application/config/movie_sessings.php */
